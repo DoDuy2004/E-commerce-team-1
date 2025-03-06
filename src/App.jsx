dbe6f83be6677bf2f86753849bd36c3d573d4930
DoDuy2004 <duy2004.gi@gmail.com>
2025-03-07 06:09:57 +0700

    [Duy] Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <>
-      <Header />
-      <Navbar />
+      <div className="py-5 border-b-1 border-[#e8e8e8] px-16">
+        <Header />
+        <Navbar />
+      </div>
+      <Banner />
     </>
   );
 }
